b1964f505e2ccf35c70b2540afcab9a2a0ee4a64
dhanish-np1 <dhanishd947@gmail.com>
2023-11-21 18:30:33 +0530

    add product completed

--- controllers/productcontrollers.js
+++ controllers/productcontrollers.js
@@ -1,6 +1,10 @@
 const { render } = require("ejs");
 const product = require("../models/productmodel")
 const category=require("../models/catogerymodel")
+const Sharp = require('sharp');
+
+
+
 
 
 
@@ -33,49 +37,43 @@ const loadproducts=async(req,res)=>{
 
 
 const addProduct = async (req, res) => {
-    try {
-      const already = await product.findOne({ name: req.body.name });
-      if(already){
-        req.session.proNameAlready = true;
-        res.redirect('/admin/addProduct')
-      }else{
-        let details = req.body;
-        const files = await req.files;
-  
-        const img = [
-          files.image1[0].filename,
-          files.image2[0].filename,
-          files.image3[0].filename,
-          files.image4[0].filename,
-        ];
-  
-        for (let i = 0; i < img.length; i++) {
-          await Sharp("public/products/images/" + img[i])
-            .resize(500, 500)
-            .toFile("public/products/crop/" + img[i]);
-        }
-  
-        let product = new product({
-          name: details.name,
-          price: details.price,
-          quantity: details.quantity,
-          category: details.category,
-          description: details.description,
-          blocked: 0,
-          "images.image1": files.image1[0].filename,
-          "images.image2": files.image2[0].filename,
-          "images.image3": files.image3[0].filename,
-          "images.image4": files.image4[0].filename,
-        });
-  
-        let result = await product.save();
-        res.redirect("/admin/product");
-      }
-      
-    } catch (error) {
-      console.log(error.message);
-      res.status(500).json({ error: 'Internal server error' });
+  const already = await product.findOne({ name: req.body.name });
+  if(!already){
+    req.session.proNameAlready = true;
+    res.redirect('/admin/addProduct')
+  }else{
+    let details = req.body;
+    const files = await req.files;
+
+    const img = [
+      files.image1[0].filename,
+      files.image2[0].filename,
+      files.image3[0].filename,
+      files.image4[0].filename,
+    ];
+
+    for (let i = 0; i < img.length; i++) {
+      await Sharp("public/products/images/" + img[i])
+        .resize(500, 500)
+        .toFile("public/products/crop/" + img[i]);
     }
+
+    let products = new product({
+      name: details.name,
+      price: details.price,
+      quantity: details.quantity,
+      category: details.category,
+      description: details.description,
+      blocked: 0,
+      "images.image1": files.image1[0].filename,
+      "images.image2": files.image2[0].filename,
+      "images.image3": files.image3[0].filename,
+      "images.image4": files.image4[0].filename,
+    });
+
+    let result = await products.save();
+    res.redirect("/admin/users");
+  }
   };
 
 module.exports = {
